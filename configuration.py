f29f146108932acda1cc4d1050bf484222ddf352
Neonicsa <neonicsa@yandex.ru>
2023-05-29 09:21:17 +0300

    Change test_create_order.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 # Адрес приложения url
-URL_SERVICE = "https://f6e9c529-02c5-4f80-990b-d439a1902d3c.serverhub.praktikum-services.ru"
+URL_SERVICE = " https://937ebdb2-f2d6-4717-9d77-e4851a9bc0db.serverhub.praktikum-services.ru"
 ORDER_CREATE = "/api/v1/orders"
 ORDER_GET = "/api/v1/orders/track"
